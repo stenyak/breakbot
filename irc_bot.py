ae9c1d94974370fe22a57bf2c7d27b76e01d0045
Bruno Gonzalez <stenyak@stenyak.com>
2012-10-24 22:04:44 +0200

    Logging of messages on receive.

--- irc_bot.py
+++ irc_bot.py
@@ -3,23 +3,27 @@
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 from oyoyo.client import IRCClient
 from oyoyo.cmdhandler import DefaultCommandHandler
+from message import Message
 
-class MyHandler(DefaultCommandHandler):
+        
+class Handler(DefaultCommandHandler):
+    def log_msg(self, message, file_path=None):
+        if file_path is None:
+            raise Exception("No file specified!")
+        print ("Logging: %s" %message)
+        text = message.serialize()
+        with open(file_path, "a") as log:
+            log.write(text)
+        
     # Handle messages (the PRIVMSG command, note lower case)
-    def privmsg(self, nick, chan, msg):
-        target = None
-        try:
-            split = msg.split(":", 1)
-            msg = split[1].lstrip()
-            target = split[0]
-        except IndexError:
-            pass
-        print "%s in %s said to %s: %s" % (nick, chan, target, msg)
+    def privmsg(self, nick_full, chan, msg):
+        m = Message(nick_full, chan, msg)
+        self.log_msg(m, "/tmp/log.txt")
 
 host = "irc.freenode.net"
 port = 6667
 nick = "my_bot"
-cli = IRCClient(MyHandler, host=host, port=port, nick=nick)
+cli = IRCClient(Handler, host=host, port=port, nick=nick)
 
 conn = cli.connect()
 print ""
