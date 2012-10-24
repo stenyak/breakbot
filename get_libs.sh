57b3c74503c8894b3664e03dc4f966e166c38f2f
Bruno Gonzalez <stenyak@stenyak.com>
2012-10-24 19:39:23 +0200

    Added getlibs script.

---
+++ get_libs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Copyright 2012 Bruno Gonzalez
+# This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
+svn checkout http://oyoyo.googlecode.com/svn/trunk/ oyoyo -r55
