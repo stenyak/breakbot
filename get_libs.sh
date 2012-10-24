d1f2ca5704fe9a2cee3ca047c34e15275bb9d0f8
Bruno Gonzalez <stenyak@stenyak.com>
2012-10-24 20:04:07 +0200

    Modified oyoyo svn path.

--- get_libs.sh
+++ get_libs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
-svn checkout http://oyoyo.googlecode.com/svn/trunk/ oyoyo -r55
+svn checkout http://oyoyo.googlecode.com/svn/trunk/oyoyo/oyoyo -r55
