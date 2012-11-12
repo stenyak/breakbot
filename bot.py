868cd01cb44cd91149d7bd061d8957ef79b40c76
Bruno Gonzalez <stenyak@stenyak.com>
2012-11-12 19:21:11 +0100

    Initial working wa bot.

--- bot.py
+++ bot.py
@@ -2,9 +2,11 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 
+import threading
 import time
 from irc_bot import IRCInterface
-#from wa_bot import WAInterface
+from wa_bot import WAInterface
+import os
 
 def store_msg(message, file_path=None):
     if file_path is None:
@@ -13,30 +15,42 @@ def store_msg(message, file_path=None):
     with open(file_path, "a") as log:
         log.write(text)
 
-def irc_msg_received(message):
-    store_msg(message, "/tmp/log.txt")
-    print "Received irc message: %s" %message
-
-def wa_msg_received(message):
-    print "Received wa message: %s" %message
-
-bot_running = False
-def bot_stopped():
-    bot_running = False
     
-def main():
-    irc_i = IRCInterface("irc.freenode.net", 6667, "my_bot", ["#sample_room"], irc_msg_received, bot_stopped)
-    bot_running = True
-    irc_i.start()
-    seconds = 0
-    try:
-        while bot_running:
+class Bot(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.must_run = False
+        self.irc_i = IRCInterface("irc.freenode.net", 6667, "my_bot", ["#sample_room"], self.irc_msg_received, self.stop)
+        DEFAULT_CONFIG = os.path.expanduser("~")+"/.yowsup/auth"
+        self.wa_i = WAInterface(DEFAULT_CONFIG, self.wa_msg_received, self.stop)
+    def run(self):
+        self.must_run = True
+        self.irc_i.start()
+        self.wa_i.start()
+        seconds = 0
+        while self.must_run:
             time.sleep(0.5)
-    except KeyboardInterrupt:
-        print "User wants to stop"
-    irc_i.stop()
+        self.irc_i.stop()
+        self.wa_i.stop()
+    def stop(self):
+        self.must_run = False
+
+    def irc_msg_received(self, message):
+        store_msg(message, "/tmp/log.txt")
+        print " >>> Received irc message: %s" %message
+        #if message.chan == "#sample_room":
+        self.wa_i.send("34555555373", "received %s" %message)
+
+    def wa_msg_received(self, message):
+        print " >>> Received wa message: %s" %message
+
+
 
 
 print "Program started"
-main()
+b = Bot()
+try:
+    b.run()
+except KeyboardInterrupt:
+    print "User wants to stop"
 print "Program finished"
--- wa_bot.py
+++ wa_bot.py
@@ -2,6 +2,8 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 import threading
+from message import Message
+from timestamp import Timestamp
 
 from yowsup.Examples import ListenerClient as ListenerClient
 from yowsup.Examples.CmdClient import WhatsappCmdClient as WhatsappCmdClient
@@ -11,36 +13,48 @@ import datetime
 import time
 
 class WAInterface(threading.Thread):
-    def __init__(self, server, port, nick, handler):
+    def __init__(self, auth_file, msg_handler, stopped_handler):
         threading.Thread.__init__(self)
         self.must_run = False
-        self.handler = handler
-        DEFAULT_CONFIG = os.path.expanduser("~")+"/.yowsup/auth"
-        def getCredentials(config = DEFAULT_CONFIG):
-            if os.path.isfile(config):
-                f = open(config)
+        self.msg_handler = msg_handler
+        self.stopped_handler = stopped_handler
+        def getCredentials(auth_file):
+            if os.path.isfile(auth_file):
+                f = open(auth_file)
                 l = f.readline().strip()
                 auth = l.split(":", 1)
                 if len(auth) == 2:
                     return auth
                 return 0
-        credentials = getCredentials(DEFAULT_CONFIG)
+        credentials = getCredentials(auth_file)
         username, identity = credentials
         password = Utilities.getPassword(identity)
-        wa = WhatsappCmdClient(True, False)
-        wa.signalsInterface.registerListener("message_received", onMessageReceived)
-        wa.methodsInterface.call("auth_login", (username, password))
+        self.username = username
+        self.identify = identity
+        self.password = password
+        self.wa = WhatsappCmdClient(True, False)
+        self.wa.signalsInterface.registerListener("message_received", self.onMessageReceived)
+        self.signals = y.getSignalsInterface()
+        self.methods = y.getMethodsInterface()
     def onMessageReceived(self, messageId, jid, messageContent, timestamp, wantsReceipt):
         try:
-            print "message received: %s" % messageContent
-            formattedDate = datetime.datetime.fromtimestamp(timestamp/1000).strftime('%d-%m-%Y %H:%M')
-            print "%s:%s"%(jid, formattedDate, messageContent)
+            message = Message(self.username, jid, messageContent)
+            message.time = Timestamp(ms_int = timestamp*1000)
+            self.msg_handler(message)
         except Exception,e:
-            print "Shit happened: %s" %e
+            print "Error while handling message: %s" %e
 
     def run(self):
         self.must_run = True
+        self.wa.methodsInterface.call("auth_login", (self.username, self.password))
         while self.must_run:
             time.sleep(0.5)
+        self.stopped_handler()
     def stop(self):
         self.must_run = False
+    def send(self, target, text):
+        print " >>> Sending WA message: %s" %message
+        message = ("%s@s.whatsapp.net"%target, text)
+        self.wa.methodsInterface.call("message_id", message)
+        print " >>> Sent!"
+        print " >>> Sending WA message: %s" %message
