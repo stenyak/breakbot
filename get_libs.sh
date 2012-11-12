4335714aade875e35f2f3fdafa6324e7575b1591
Bruno Gonzalez <stenyak@stenyak.com>
2012-11-12 18:47:33 +0100

    Changed deps getter.

--- get_libs.sh
+++ get_libs.sh
@@ -2,4 +2,5 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 svn checkout http://oyoyo.googlecode.com/svn/trunk/oyoyo/oyoyo -r55
-git clone https://github.com/tgalal/yowsup.git
+git clone https://github.com/tgalal/yowsup.git yowsup.git
+ln -s yowsup.git/src yowsup
