d975c45330ad49a71be9573be039dcf039fa1fd3
Bruno Gonzalez <stenyak@stenyak.com>
2013-03-19 15:43:18 +0100

    Version symlink to yowsup.

--- get_libs.sh
+++ get_libs.sh
@@ -2,4 +2,3 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 svn checkout http://oyoyo.googlecode.com/svn/trunk/oyoyo/oyoyo -r55
-ln -s yowsup.git/src/Yowsup .
