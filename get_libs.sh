d2c9150171db4a24a5dd9632377730b5789e07f0
Bruno Gonzalez <stenyak@stenyak.com>
2013-11-13 11:14:56 +0100

    Also get git submodules.
    Fixes https://github.com/stenyak/breakbot/issues/9

--- get_libs.sh
+++ get_libs.sh
@@ -2,3 +2,4 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 svn checkout http://oyoyo.googlecode.com/svn/trunk/oyoyo/oyoyo -r55
+git submodule update --init
