8fcb8c5778e63c7badec3a5b49b89b225ed6d873
Bruno Gonzalez <stenyak@stenyak.com>
2014-01-28 21:43:21 +0100

    Remove unnecessary decoding of incoming private message. Fixes #14

--- wa_bot.py
+++ wa_bot.py
@@ -40,7 +40,6 @@ class WAInterface(threading.Thread):
         self.signalsInterface.registerListener("ping", self.onPing)
     @catch_them_all
     def onMessageReceived(self, messageId, jid, messageContent, timestamp, wantsReceipt, pushName):
-        messageContent = unicode(messageContent, "utf-8")
         message = Message(kind="wa", nick_full=jid, chan=self.username, msg=messageContent)
         message.time = Timestamp(ms_int = timestamp*1000)
         self.msg_handler(message)
