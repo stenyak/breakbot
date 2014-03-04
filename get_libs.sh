6811ff8ed13ad973677463afd095380f3aaec099
Bruno Gonzalez <stenyak@stenyak.com>
2014-03-04 09:08:03 +0100

    Exit on first fail.

--- get_libs.sh
+++ get_libs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
+
+set -e
+
 svn checkout http://oyoyo.googlecode.com/svn/trunk/oyoyo/oyoyo -r55
 git submodule update --init
