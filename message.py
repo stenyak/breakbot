a73706ecdf005ec886d7a86c1c1d23e93fa35d4f
Bruno Gonzalez <stenyak@stenyak.com>
2014-09-08 12:18:18 +0200

    refs #24 Correctly detects ":" target user in messages.
    Should stop dropping smileys and similar stuff.

--- message.py
+++ message.py
@@ -1,6 +1,23 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 from timestamp import Timestamp
+import re
+
+def sed(text, regexp):
+    m = re.search(regexp, text)
+    if m:
+        return m.groups()[0]
+    else:
+        return None
+
+def split_nick(text):
+    target = sed(text, r"^\s*([\w0-9+]+)\s*:\s*.*")
+    if target in (None, "ftp", "ssh", "http","https","image"):
+        return [None, text]
+    msg = sed(text, r"^\s*[\w0-9+]+\s*:\s*(.*)")
+    if msg.startswith((")", "(", "/", "-")):
+        return [None, text]
+    return [target, msg]
 
 class Message():
     def __init__(self, kind=None, nick_full=None, chan=None, msg=None, time=None, serialized_str=None):
@@ -14,16 +31,7 @@ class Message():
             self.kind = kind
             self.nick_full = nick_full
             self.chan = chan
-            self.msg = msg
-            self.target = None
-            try:
-                split = msg.split(":", 1)
-                if len(split) == 2:
-                    if not split[0].endswith(("http","https","image")):
-                        self.target = split[0]
-                        self.msg = split[1].lstrip()
-            except IndexError:
-                pass
+            self.target, self.msg = split_nick(msg)
         else:
             self.deserialize(serialized_str)
     def get_nick(self):
@@ -35,7 +43,7 @@ class Message():
     def __str__(self):
         return ("%s: %s in %s said to %s: %s" % (self.time, self.get_nick(), self.chan, self.target, self.msg)).encode("utf-8")
     def serialize(self):
-        return " @@@ ".join([str(self.time), self.kind, self.nick_full, self.chan, self.msg])
+        return " @@@ ".join([str(self.time), self.kind, self.nick_full, self.chan, self.msg if self.target is None else "%s: %s"%(self.target, self.msg)])
     def deserialize(self, string):
         fields = string.split(" @@@ ")
         time = Timestamp(ms_str=fields[0])
--- message.test.py
+++ message.test.py
@@ -2,7 +2,7 @@
 # Copyright 2012 Bruno Gonzalez
 # This software is released under the GNU AFFERO GENERAL PUBLIC LICENSE (see agpl-3.0.txt or www.gnu.org/licenses/agpl-3.0.html)
 import unittest
-from message import Message
+from message import Message, split_nick, sed
 from timestamp import Timestamp
 
 class TestMessage(unittest.TestCase):
@@ -31,6 +31,47 @@ class TestMessage(unittest.TestCase):
         self.assertEquals(m.serialize(), m2.serialize())
         m3 = Message(serialized_str="10 @@@ irc @@@ complete_nick!foobar @@@ channel_name @@@ BaZ: message contents")
         self.assertEquals(m.serialize(), m3.serialize())
+
+    def test_sed(self):
+        self.assertEquals(sed("foobar", "(foo)"), "foo")
+        self.assertEquals(sed("bar 123 baz", r"bar (\d+) baz"), "123")
+        self.assertEquals(sed("bar 123 baz", r"bar (\d+) "),    "123")
+        self.assertEquals(sed("bar 123 baz", r"^bar (\d+) "),   "123")
+        self.assertEquals(sed("bar 123 baz", r"^.* (\d+)"),     "123")
+        self.assertEquals(sed("bar 123 baz", r"^.* (\d+).*$"),  "123")
+        self.assertEquals(sed("  123   \t", r"^\s*(\d*)\s*$"),  "123")
+        self.assertEquals(sed("  foo123bar   ", r"^\s*([a-z0-9]*)\s*$"),    "foo123bar")
+        self.assertEquals(sed("  foo123BAR   ", r"^\s*([a-zA-Z0-9]*)\s*$"), "foo123BAR")
+        self.assertEquals(sed("  foo123ba+r   ", r"^\s*([a-z0-9+]*)\s*$"),  "foo123ba+r")
+        self.assertEquals(sed("  foo123ba+r   ", r"^\s*([a-z0-9+]*)\s*$"),  "foo123ba+r")
+        self.assertEquals(sed("  foo   ", r"\s*([a-z]*)\s*"), "foo")
+        self.assertEquals(sed("  foo   ", r"\s(\w+)\s"), "foo")
+        self.assertEquals(sed("  foo   ", r"\s*\s(\w+)\s*"), "foo")
+        self.assertEquals(sed("foo:hey", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed(" foo:hey", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed("foo :hey", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed("foo:hey ", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed(" foo : hey ", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed("  foo  :  hey  ", r"\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed("  foo  :  hey  ", r"^\s*(\w+)\s*:"), "foo")
+        self.assertEquals(sed("  foo_bar  :  hey  ", r"^\s*(\w+)\s*:"), "foo_bar")
+        self.assertEquals(sed("  +34555555  :  hey  ", r"^\s*([\w0-9+]+)\s*:"), "+34555555")
+
+    def test_split(self):
+        self.assertEquals(split_nick("foo bar"), [None, "foo bar"])
+        self.assertEquals(split_nick("foo:bar"), ["foo", "bar"])
+        self.assertEquals(split_nick("foo:"), ["foo", ""])
+        self.assertEquals(split_nick(" foo : bar "), ["foo", "bar "])
+        self.assertEquals(split_nick("ftp://foobar"), [None, "ftp://foobar"])
+        self.assertEquals(split_nick("ftp://foo:bar"), [None, "ftp://foo:bar"])
+        self.assertEquals(split_nick("ftp://foo:bar"), [None, "ftp://foo:bar"])
+        self.assertEquals(split_nick(":)"), [None, ":)"])
+        self.assertEquals(split_nick("hey :)"), [None, "hey :)"])
+        self.assertEquals(split_nick("foo: whats up? :)"), ["foo", "whats up? :)"])
+        self.assertEquals(split_nick("foo: whats up? :("), ["foo", "whats up? :("])
+        self.assertEquals(split_nick("foo: whats up? :/"), ["foo", "whats up? :/"])
+        self.assertEquals(split_nick("foo: whats up? :-/"), ["foo", "whats up? :-/"])
+        self.assertEquals(split_nick("foo: check this url: ftp://foo :-)"), ["foo", "check this url: ftp://foo :-)"])
 
 if __name__ == '__main__':
     unittest.main()
